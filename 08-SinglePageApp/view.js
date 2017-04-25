514cb964eb24b51b60eb0d1a2d53492954a2b72a
s-116-26 <s-116-26@gumiforest.ae.krakow.pl>
2017-04-25 18:07:28 +0200

    Single page apps part1

--- 08-SinglePageApp/view.js
+++ 08-SinglePageApp/view.js
@@ -1,7 +1,38 @@
 var view = {
 		display: function (data) {
+            
+            var table, header, td, tdt, row;
+            var hrs = ["Code", "Name", "Rate"];
+            var rws = [data[i].code]
+            this.clear();
+            
+            table = document.createElement("table");
+            table.setAttribute("border", 1);
+            header = document.createElement("hr");
+            
+            for(var i=0; i<3; i++) {
+                td = document.createElement("td");
+                tdt = document.createTextNode(hrs[i]);
+                td.appendChild(tdt);
+                header.appendChild(td);
+            }
+            
+            for(var i=0; i<data.length; i++) {
+                row = document.createElement("tr");
+                for(var j=0; j<3; j++) {
+                    td = document.createElement("td");
+                    tdt = document.createTextNode();
+                    td.appendChild(tdt);
+                }
+            }
+            
+            table.appendChild(header);
+            
+            
+            document.getElementById('dataview').appendChild(table);
+            
 			// create new data view to be displayed
-			var elem, text, i;
+            /*var elem, text, i;
 			this.clear();
 			for (i = 0; i < data.length; i++) {
 				elem = document.createElement("h3");
@@ -10,7 +41,7 @@ var view = {
 						);
 				elem.appendChild(text);
 				document.getElementById('dataview').appendChild(elem);
-			}
+			}*/
 		},
 		clear: function () {
 			// remove old data
